f086d15b8075a380dedc62f3eb81958880e7f9dd
professorjiwon <professorjiwon@gmail.com>
2025-05-13 12:52:22 +0900

    dart_집합함수변환

--- dart_basic/bin/21_inheritance.dart
+++ dart_basic/bin/21_inheritance.dart
@@ -3,11 +3,16 @@ void main() {
   bts.sayName();
   bts.sayMemberCount();
   bts.sayMale();
+  print(bts.calulate());
 
   GirlGroup blackpink = GirlGroup('BlackPink', 5);
   blackpink.sayName();
   blackpink.sayMemberCount();
   blackpink.sayFemale();
+
+  print('------------- Type Comparison ---------------');
+  print(bts is Idol);
+  print(bts is BoyGroup);
 }
 
 class Idol {
@@ -23,6 +28,10 @@ class Idol {
   sayMemberCount() {
     print('$name은 $memberCount명의 멤버로 구성되어 있습니다.');
   }
+
+  int calulate() {
+    return memberCount * 2;
+  }
 }
 
 class BoyGroup extends Idol {
@@ -35,6 +44,12 @@ class BoyGroup extends Idol {
   // 함수선언
   sayMale() {
    print('저희는 남자 아이돌 입니다');
+  }
+
+  @override
+  int calulate() {
+    // return memberCount * 4;
+    return super.calulate() * 4;
   }
 }
 
