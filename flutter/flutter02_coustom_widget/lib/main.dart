618b3da756bd619aec1d8987c2424bb946b2eb34
professorjiwon <professorjiwon@gmail.com>
2025-05-15 12:53:03 +0900

    flutter_ListView_ListTile

--- flutter/flutter02_coustom_widget/lib/main.dart
+++ flutter/flutter02_coustom_widget/lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
 }
 */
 
+/*
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -46,13 +47,67 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(backgroundColor: Color(0xFFf3edf7)),
         body: ListView(
           children: [
-            
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset('assets/user1.png', width: 50,),
+                    SizedBox(width: 30,),
+                    Text('홍길동')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset('assets/user2.png', width: 50,),
+                    SizedBox(width: 30,),
+                    Text('더조은')
+                  ],
+                )
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
+*/
+
+// ListView에서 ListTile(): 같은 layout을 여러개 생성할 때 유용
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Color(0xFFf3edf7)),
+        /*
+        body: ListView(
+          children: [
+            ListTile(
+              leading: Image.asset('assets/user1.png'),
+              title: Text('홍길동'),
+            ),
+            ListTile(
+              leading: Image.asset('assets/user2.png'),
+              title: Text('이한빛'),
+            )
+          ],
+        )
+        */
+
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Text('반복됨 $index');
+          },
+          itemCount: 5,
+        ),
+      ),
+    );
+  }
+}
+
 
 
 
